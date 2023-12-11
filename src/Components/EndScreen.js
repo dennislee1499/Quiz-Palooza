6d1ba9e07fe814d45cbb1cde3216e05a80ac71ce
Dennis Lee <dennislee1499@gmail.com>
2023-12-11 00:51:45 -0500

    EndScreen - restart quiz/ show final score

--- src/Components/EndScreen.js
+++ src/Components/EndScreen.js
@@ -1,10 +1,23 @@
+import { QuizContext } from "../Helpers/Context";
+import { useContext } from "react";
+import { Questions } from "../Helpers/QuestionBank";
+import '../EndScreen.css';
 
 function EndScreen() {
-    return (
-        <div>
+    const { score, setScore, setGameState } = useContext(QuizContext);
+    
+    const restartQuiz = () => {
+        setScore(0);
+        setGameState("menu");
+    }
 
-        </div>
-    )
-}
+  return (
+    <div className="EndScreen">
+        <h1> Quiz Finished! </h1>
+        <h3> {score} / {Questions.length} </h3>
+        <button onClick={restartQuiz}> Restart Quiz </button>
+    </div>
+  );
+};
 
 export default EndScreen;
